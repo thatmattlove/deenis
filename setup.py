d353cc945c9f11ff8024daef2fb5b3cb37e8db0b
checktheroads <mtt.lov@gmail.com>
2019-06-26 12:32:34 -0700

    Add install_requires

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ setup(
     version="0.0.1",
     python_requires=">=3.6.2",
     packages=["deenis"],
+    install_requires=[
+        "click>=6.7",
+        "logzero>=1.5.0",
+        "requests>=2.21.0",
+        "toml>=0.10.0",
+    ],
     license="BSD 3-Clause Clear License",
     long_description=open("README.md").read(),
 )
